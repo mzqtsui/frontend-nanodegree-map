3e06f4e371d9d7238dc34ba26c5b5feb0e90dd72
Matthew Tsui <matthew.zq.tsui@gmail.com>
2016-09-14 08:29:51 -0400

    markers added

--- src/js/app.js
+++ src/js/app.js
@@ -1,16 +1,18 @@
-(function () {
+(function() {
     "use strict";
 
     class Gym {
         constructor (gi) {
             this.uberInfo = {};
             this.generalInfo = gi;
+            //TODO: add rates manually
 
         }
     }
 
-    var ViewModel = function (dataList) {
+    var ViewModel = function(dataList) {
         var self = this;
+        this.searchFocused = ko.observable(false);
         this.currentLocation = ko.observable(null);
         this.query = ko.observable("");
         this.gymList = ko.observableArray(dataList);
@@ -26,16 +28,18 @@
             }
         });
 
-        this.submitForm = function () {
+        this.submitForm = function() {
             self.selectFirstLocation();
         };
 
-        this.selectLocation = function () {
+        this.selectLocation = function() {
             self.currentLocation(this);
             self.query(this.generalInfo.name);
+            self.searchFocused(true);
+            console.log(self.searchFocused());
         };
 
-        this.selectFirstLocation = function () {
+        this.selectFirstLocation = function() {
             if(self.filterLocations().length > 0 && self.query().length > 0) {
                 self.currentLocation(self.filterLocations()[0]);
                 self.query(self.currentLocation().generalInfo.name);
@@ -46,9 +50,10 @@
 
         };
 
-        this.clearSearch = function () {
+        this.clearSearch = function() {
             self.currentLocation(null);
             self.query("");
+            self.searchFocused(true);
         };
     };
 
@@ -56,7 +61,7 @@
 
 
 
-    function getData () {
+    function getData() {
         return [
             {
                 name: "Planet Granite San Francisco",
@@ -101,7 +106,7 @@
         ];
     }
 
-    function parseData (dataList) {
+    function parseData(dataList) {
         var ret = [];
         for(var i = 0, len = dataList.length; i < len; i++) {
             ret.push(new Gym(dataList[i]));
@@ -115,3 +120,53 @@
     //TODO: Uber API
 
 })();
+
+var markers = [];
+
+function initMap() {
+    // Create a map object and specify the DOM element for display.
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 37.6419628, lng: -122.5127333},
+        zoom: 10
+    });
+
+    var locations = [
+        {
+            title: "Planet Granite San Francisco",
+            position: {lat: 37.8041267, lng: -122.4685219}
+        },
+        {
+            title: "Dogpatch Boulders",
+            position: {lat: 37.7566538, lng: -122.389994}
+        },
+        {
+            title: "Mission Cliffs",
+            position: {lat: 37.7610241, lng: -122.4147971}
+        },
+        {
+            title: "Great Western Power Company",
+            position: {lat: 37.8255233, lng: -122.2887678}
+        },
+        {
+            title: "Berkeley Ironworks",
+            position: {lat: 37.8508457, lng: -122.2947798}
+        }
+    ];
+
+    var bounds = new google.maps.LatLngBounds();
+
+    for(var i = 0, len = locations.length, marker; i < len; i++) {
+        marker = new google.maps.Marker({
+            position: locations[i].position,
+            map: map,
+            title: locations[i].title,
+            id: i
+        });
+        markers.push(marker);
+
+        bounds.extend(marker.position);
+    }
+
+    map.fitBounds(bounds);
+
+}
