574172bd7859c9b229917888e3edb7c141846d48
Matthew Tsui <matthew.zq.tsui@gmail.com>
2016-09-14 08:55:11 -0400

    markers linked to filter

--- src/js/app.js
+++ src/js/app.js
@@ -1,154 +1,106 @@
-(function() {
-    "use strict";
+var map;
 
-    class Gym {
-        constructor (gi) {
-            this.uberInfo = {};
-            this.generalInfo = gi;
-            //TODO: add rates manually
 
-        }
+class Gym {
+    constructor (gi, marker) {
+        this.uberInfo = {};
+        this.generalInfo = gi;
+        this.marker = marker;
+        //TODO: add rates manually
+
     }
+}
 
-    var ViewModel = function(dataList) {
-        var self = this;
-        this.searchFocused = ko.observable(false);
-        this.currentLocation = ko.observable(null);
-        this.query = ko.observable("");
-        this.gymList = ko.observableArray(dataList);
+var ViewModel = function(dataList) {
+    var self = this;
+    this.searchFocused = ko.observable(false);
+    this.currentLocation = ko.observable(null);
+    this.query = ko.observable("");
+    this.gymList = ko.observableArray(dataList);
 
-        this.filterLocations =  ko.computed(function () {
-            var filter = self.query().toLowerCase();
-            if (!filter) {
-                return self.gymList();
-            } else {
-                return ko.utils.arrayFilter(self.gymList(), function (item) {
-                    return item.generalInfo.name.toLowerCase().indexOf(filter) !== -1;
-                });
-            }
-        });
+    this.filterLocations =  ko.computed(function () {
+        var filter = self.query().toLowerCase();
+        if (!filter) {
+            self.gymList().forEach(function(item) {
+                item.marker.setVisible(true);
+            });
+            return self.gymList();
+        } else {
+            return ko.utils.arrayFilter(self.gymList(), function (item) {
+                var shown = item.generalInfo.name.toLowerCase().indexOf(filter) !== -1;
+                item.marker.setVisible(shown);
+                return shown;
+            });
+        }
+    });
 
-        this.submitForm = function() {
-            self.selectFirstLocation();
-        };
-
-        this.selectLocation = function() {
-            self.currentLocation(this);
-            self.query(this.generalInfo.name);
-            self.searchFocused(true);
-            console.log(self.searchFocused());
-        };
-
-        this.selectFirstLocation = function() {
-            if(self.filterLocations().length > 0 && self.query().length > 0) {
-                self.currentLocation(self.filterLocations()[0]);
-                self.query(self.currentLocation().generalInfo.name);
-                console.log("select");
-            }
-            //TODO show error not found
-
-
-        };
-
-        this.clearSearch = function() {
-            self.currentLocation(null);
-            self.query("");
-            self.searchFocused(true);
-        };
+    this.submitForm = function() {
+        self.selectFirstLocation();
     };
 
-    //TODO: Google Maps API
+    this.selectLocation = function() {
+        self.currentLocation(this);
+        self.query(this.generalInfo.name);
+        self.searchFocused(true);
+        console.log(self.searchFocused());
+    };
 
-
-
-    function getData() {
-        return [
-            {
-                name: "Planet Granite San Francisco",
-                address: "924 Old Mason St, San Francisco, CA 94129, United States",
-                location: [37.6419628, -122.5127333],
-                url: "planetgranite.com",
-                phone: "+1 415-692-3434",
-                hours: "6AM - 11PM"
-            },
-            {
-                name: "Dogpatch Boulders",
-                address: "924 Old Mason St, San Francisco, CA 94129, United States",
-                location: [37.6419628, -122.5127333],
-                url: "planetgranite.com",
-                phone: "+1 415-692-3434",
-                hours: "6AM - 11PM"
-            },
-            {
-                name: "Mission Cliffs",
-                address: "924 Old Mason St, San Francisco, CA 94129, United States",
-                location: [37.6419628, -122.5127333],
-                url: "planetgranite.com",
-                phone: "+1 415-692-3434",
-                hours: "6AM - 11PM"
-            },
-            {
-                name: "Great Western Power Company",
-                address: "924 Old Mason St, San Francisco, CA 94129, United States",
-                location: [37.6419628, -122.5127333],
-                url: "planetgranite.com",
-                phone: "+1 415-692-3434",
-                hours: "6AM - 11PM"
-            },
-            {
-                name: "Berkeley Ironworks",
-                address: "924 Old Mason St, San Francisco, CA 94129, United States",
-                location: [37.6419628, -122.5127333],
-                url: "planetgranite.com",
-                phone: "+1 415-692-3434",
-                hours: "6AM - 11PM"
-            }
-        ];
-    }
-
-    function parseData(dataList) {
-        var ret = [];
-        for(var i = 0, len = dataList.length; i < len; i++) {
-            ret.push(new Gym(dataList[i]));
+    this.selectFirstLocation = function() {
+        if(self.filterLocations().length > 0 && self.query().length > 0) {
+            self.currentLocation(self.filterLocations()[0]);
+            self.query(self.currentLocation().generalInfo.name);
+            console.log("select");
         }
-        return ret;
-    }
-
-    ko.applyBindings(new ViewModel(parseData(getData())));
+        //TODO show error not found
 
 
-    //TODO: Uber API
+    };
 
-})();
+    this.clearSearch = function() {
+        self.currentLocation(null);
+        self.query("");
+        self.searchFocused(true);
+    };
+};
+
+//TODO: Google Maps API
+
+
+//TODO: Uber API
 
 var markers = [];
 
 function initMap() {
     // Create a map object and specify the DOM element for display.
-    var map = new google.maps.Map(document.getElementById('map'), {
+    map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 37.6419628, lng: -122.5127333},
         zoom: 10
     });
 
     var locations = [
         {
-            title: "Planet Granite San Francisco",
+            name: "Planet Granite San Francisco",
+            placeId: "ChIJYR3cR92GhYAReMwzpHwVcI8",
             position: {lat: 37.8041267, lng: -122.4685219}
         },
         {
-            title: "Dogpatch Boulders",
+            name: "Dogpatch Boulders",
+            placeId: "ChIJ2xYuRrp_j4ARU9Lx0LBnqrY",
             position: {lat: 37.7566538, lng: -122.389994}
         },
         {
-            title: "Mission Cliffs",
+            name: "Mission Cliffs",
+            placeId: "ChIJQ2qEqDB-j4ARfAbDE0-IWvo",
             position: {lat: 37.7610241, lng: -122.4147971}
         },
         {
-            title: "Great Western Power Company",
+            name: "Great Western Power Company",
+            placeId: "ChIJiZODxK2Aj4ARTMot5wllmGU",
             position: {lat: 37.8255233, lng: -122.2887678}
         },
         {
-            title: "Berkeley Ironworks",
+            name: "Berkeley Ironworks",
+            placeId: "ChIJzXxD1PV-hYAR4VEMx86aHVw",
             position: {lat: 37.8508457, lng: -122.2947798}
         }
     ];
@@ -159,7 +111,7 @@ function initMap() {
         marker = new google.maps.Marker({
             position: locations[i].position,
             map: map,
-            title: locations[i].title,
+            name: locations[i].name,
             id: i
         });
         markers.push(marker);
@@ -168,5 +120,14 @@ function initMap() {
     }
 
     map.fitBounds(bounds);
+
+
+    var data = [];
+    for(var i = 0, len = locations.length; i < len; i++) {
+        data.push(new Gym(locations[i], markers[i]));
+    }
+
+    ko.applyBindings(new ViewModel(data));
+
 
 }
